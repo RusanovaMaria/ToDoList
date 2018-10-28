462746a68c7416fd3914f87277223a252b4350eb
Rusanova Maria <mariarusanova1997@mail.ru>
2018-10-28 22:50:34 +0300

    Добавлена основная структура приложения

---
+++ app/src/main/java/com/rusanova/todolist/NoteFragment.java
@@ -0,0 +1,22 @@
+package com.rusanova.todolist;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public  class NoteFragment extends Fragment {
+    public static final String ARG_CAT_NUMBER = "cat_number";
+
+    public NoteFragment() {
+        // Для фрагмента требуется пустой конструктор
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return null;
+    }
+}
