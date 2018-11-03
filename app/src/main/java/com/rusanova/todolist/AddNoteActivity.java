431f497587eb70e3c539627742c182c6e600d88c
Rusanova Maria <mariarusanova1997@mail.ru>
2018-11-03 21:13:25 +0300

    Исправлены баги в работе класса AddNoteActivity

--- app/src/main/java/com/rusanova/todolist/AddNoteActivity.java
+++ app/src/main/java/com/rusanova/todolist/AddNoteActivity.java
@@ -2,9 +2,15 @@ package com.rusanova.todolist;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AddNoteActivity extends AppCompatActivity {
@@ -13,8 +19,6 @@ public class AddNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
